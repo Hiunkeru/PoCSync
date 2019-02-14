d51467b4e8b5c8b5573e126dcfce01770290bf44
Alberto Picazo <alberto.picazo@brintia.com>
2019-02-14 16:00:38 +0100

    ssdswdqs

--- PocSync/Controllers/HomeController.cs
+++ PocSync/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace PocSync.Controllers
             return View();
         }
 
-        public ActionResult AboutPrueba()
+        public ActionResult AboutPrueba1212121312()
         {
             ViewBag.Message = "Your application description page.";
 
